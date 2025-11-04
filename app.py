49b1e04ba5d5013112d04535fd65da774bc599b4
Luke Davis <92871067+lukedavisseo@users.noreply.github.com>
2025-11-04 12:40:43 +0000

    Update app.py

--- app.py
+++ app.py
@@ -55,16 +55,19 @@ generate = st.button("Generate")
 if generate:
 
 	test_list = [url for url in list_b.split('\n')]
-	match_list = funcs.all_generate_matches(input_data, input_type)
+	
 	if input_type == 'List' and list_removal:
 		match_list = funcs.remove_from_list(test_list, match_list, option_picker)
 	else:
 		match_list = funcs.all_generate_matches(input_data, input_type)
 
-	if option_picker == 'From List A to List B':
-		model = PolyFuzz().match(match_list, test_list)
-	else:
-		model = PolyFuzz().match(test_list, match_list)
+	try:
+		if option_picker == 'From List A to List B':
+			model = PolyFuzz().match(match_list, test_list)
+		else:
+			model = PolyFuzz().match(test_list, match_list)
+	except ValueError:
+		st.warning("It looks like there's an empty match list now. Maybe swap the lists around and try again?")
 
 	# This converts the matches into another DataFrame
 	df = model.get_matches()
